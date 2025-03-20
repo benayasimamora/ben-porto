bd413b3b9e922c7b516a58dec4405f3deac5c57c
Benaya Simamora <benayasimamora@benben-Macbook.local>
2025-03-20 23:06:28 +0700

    coba ubah font

--- src/app/WelcomingPage/welcomesection.tsx
+++ src/app/WelcomingPage/welcomesection.tsx
@@ -10,16 +10,19 @@ export default function WelcomeSection() {
                         poppins-regular'>
             <h1 className='text-9xl
                         text-white
-                        font-medium'>
+                        font-medium
+                        poppins-regular'>
                 hai!
             </h1>
             <p className='text-2xl
-                        text-white'>
+                        text-white
+                        poppins-regular'>
                 welcome to ben portfolio
             </p>
             <p className='text-xl
                         text-white
-                        mt-2'>
+                        mt-2
+                        poppins-regular'>
                 work with heart, serve with excellence
             </p>
         </div>
